ed04ba1bab80cf54d2bf31ad2c60d96e6acc0790
qwq140 <qwq140@gmail.com>
2021-10-14 17:41:15 +0900

    3. jwt 임시 토큰 만들어서 테스트

--- src/main/java/com/cos/jwt/filter/MyFilter1.java
+++ src/main/java/com/cos/jwt/filter/MyFilter1.java
@@ -1,13 +1,32 @@
 package com.cos.jwt.filter;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
 public class MyFilter1 implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+        HttpServletRequest req = (HttpServletRequest) servletRequest;
+        HttpServletResponse res = (HttpServletResponse) servletResponse;
+
         System.out.println("필터1");
-        filterChain.doFilter(servletRequest, servletResponse);
+
+        // 토큰 : cos 이걸 만들어줘야 함. id,pw 정상적으로 들어와서 로그인이 완료되면 토큰을 만들어주고 그걸 응답을 해준다.
+        // 요청할 때 마다 header에 Authorization에 value값으로 토큰을 가지고 온다.
+        // 그때 토큰이 넘어오면 이 토큰이 내가 만든 토큰이 맞는지만 검증하면 된다. (RSA, HS256)
+        if (req.getMethod().equals("POST")){
+            String headerAuth = req.getHeader("Authorization");
+            System.out.println(headerAuth);
+            if (headerAuth.equals("cos")){
+                filterChain.doFilter(req, res);
+            } else {
+                PrintWriter out = res.getWriter();
+                out.println("인증안됨");
+            }
+        }
     }
 }
